64dc5a41508ec689969b79648fed5942ce7b951a
Joe <josephsanglee@gmail.com>
2016-10-18 11:38:24 -0700

    Migrate all middleware to a separate file

---
+++ server/config/middleware.js
@@ -0,0 +1,14 @@
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+module.exports = function(app, express) {
+  app.use(express.static(__dirname + '/../client'));
+  app.use('/bower_components',  express.static(__dirname + '/../client/bower_components'));
+  app.use(morgan('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.text());
+  app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+  app.use(methodOverride());
+};
--- server/server.js
+++ server/server.js
@@ -1,24 +1,14 @@
 var express = require('express');
-var request = require('request');
-var morgan = require('morgan');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+var middleware = require('./config/middleware.js');
+var helpers = require('./config/helpers.js');
 var port = process.env.PORT || 8080
 var app = express();
-var utils = require('./utils/helpers.js');
 
-//connect to Mongoose database at some point
-// mongoose.connect('mongodb://localhost/poppin');
 
-app.use(express.static(__dirname + '/../client'));
-app.use('/bower_components',  express.static(__dirname + '/../client/bower_components'));
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
-app.use(methodOverride());
+mongoose.connect('mongodb://localhost/poppin');
+
+middleware(app, express);
 
 
 //fetch artist data from spotify API
@@ -26,14 +16,16 @@ app.post('/api/search', function (req, res) {
   var query = req.body.name.toLowerCase().replace(/ /g, '%20').trim();
   var url = 'https://api.spotify.com/v1/search?q=' + query + '&type=artist';
 
-  utils.getArtist(url, function(artist) {
+  helpers.getArtist(url, function(artist) {
     console.log(artist);
     res.status(200).send(JSON.stringify(artist));
   });
 
 });
 
-// get/post requests for users here
+// post request for user signin
+
+// post request for user signup
 
 // get/post requests for user favorite artists here
 
@@ -42,3 +34,5 @@ app.post('/api/search', function (req, res) {
 
 app.listen(port);
 console.log('Listening on port', port);
+
+module.exports = app;
