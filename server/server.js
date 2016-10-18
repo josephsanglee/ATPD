3de03822e5205aa20c368474fb0f337cc3202664
Joe <josephsanglee@gmail.com>
2016-10-17 19:51:16 -0700

    Abstract Spotify API requests into a utilities folder

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var port = process.env.PORT || 8080
 var app = express();
+var utils = require('./utils/helpers.js');
 
 //connect to Mongoose database at some point
 // mongoose.connect('mongodb://localhost/poppin');
@@ -22,31 +23,14 @@ app.use(methodOverride());
 
 //fetch artist data from spotify API
 app.post('/api/search', function (req, res) {
-  
   var query = req.body.name.toLowerCase().replace(/ /g, '%20').trim();
   var url = 'https://api.spotify.com/v1/search?q=' + query + '&type=artist';
 
-  request(url, function(error, response, body) {
-    if (error) { res.status(404).send(error); }
-
-    var searchData = JSON.parse(body);
-    //response code 200 means the data was successfully retrieved
-    if (response.statusCode == 200) {
-      var artistData = searchData.artists.items[0];
-
-      var artist = {
-        id: artistData.id,
-        name: artistData.name,
-        popularity: artistData.popularity,
-        image: artistData.images[0].url,
-        followers: artistData.followers.total
-      }
-
-      console.log('sending artist data');
-      console.log(artist);
-      res.status(200).send(JSON.stringify(artist));
-    }
+  utils.getArtist(url, function(artist) {
+    console.log(artist);
+    res.status(200).send(JSON.stringify(artist));
   });
+
 });
 
 // get/post requests for users here
---
+++ server/utils/helpers.js
@@ -0,0 +1,84 @@
+//////////////SPOTIFY API REQUEST HELPERS////////////////////
+var request = require('request');
+
+
+var getTopTracks = function(id, cb) {
+  var query = 'https://api.spotify.com/v1/artists/' + id + '/top-tracks?country=US';
+
+  request(query, function(error, response, body) {
+    if (error) { return console.log(error); }
+
+    var tracks = JSON.parse(body).tracks;
+    tracks = tracks.map(function(track) {
+      return {
+        name: track.name,
+        albumCover: track.album.images[0].url,
+        preview: track.preview_url
+      }
+    });
+
+    cb(tracks);
+  });
+};
+
+
+
+var getRelatedArtists = function(id, cb) {
+  var query = 'https://api.spotify.com/v1/artists/' + id + '/related-artists';
+
+  request(query, function(error, response, body) {
+    if (error) { return console.log(error); }
+
+    var artists = JSON.parse(body).artists;
+    artists = artists.map(function(artist) {
+      return {
+        id: artist.id,
+        name: artist.name,
+        image: artist.images[0].url
+      }
+    });
+
+    cb(artists);
+  });
+};
+
+
+
+var getArtistData = function(url, cb) {
+  var artistData = {};
+
+  //search for artist
+  request(url, function(error, response, body) {
+    if (error) { return console.log(error); }
+
+    var searchData = JSON.parse(body);
+
+    if (searchData !== '' && response.statusCode == 200) {
+      var artist = searchData.artists.items[0];
+      artistData.name = artist.name;
+      artistData.popularity = artist.popularity;
+      artistData.image = artist.images[0].url;
+      artistData.followers = artist.followers.total;
+
+      getTopTracks(artist.id, function(topTracks) {
+        artistData.topTracks = topTracks;
+
+        getRelatedArtists(artist.id, function(relatedArtists) {
+          artistData.relatedArtists = relatedArtists;
+
+          cb(artistData);
+        });
+      });
+    }
+
+  });
+};
+
+
+
+
+
+
+module.exports = {
+  getArtist: getArtistData
+};
